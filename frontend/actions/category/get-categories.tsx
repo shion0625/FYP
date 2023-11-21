0a2174b31ee5ea742557b126c577babf07c98d10
shion0625 <xkaito0912@gmail.com>
2023-11-21 14:15:33 +0800

    actionsの整理

--- frontend/actions/category/get-categories.tsx
+++ frontend/actions/category/get-categories.tsx
@@ -3,9 +3,7 @@ import axios from "axios";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories/`;
 
-const getCategories = async (): Promise<Category[]> => {
+export const getCategories = async (): Promise<Category[]> => {
   const res = await axios.get(URL);
   return res.data.data[0];
 };
-
-export default getCategories;
