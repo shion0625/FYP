8e58047aa226b9240148868a8bf000d7ec5f2889
shion0625 <xkaito0912@gmail.com>
2023-11-28 14:22:05 +0800

    iconボタンの削除

--- frontend/components/ui/product-item-card.tsx
+++ frontend/components/ui/product-item-card.tsx
@@ -15,34 +15,20 @@ interface ProductItemCardProps {
 const ProductItemCard: React.FC<ProductItemCardProps> = ({ data }) => {
   const cart = useCart();
   const previewModal = usePreviewModal();
-  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+  const onPreview: MouseEventHandler<HTMLElement> = (event) => {
     event.stopPropagation();
 
     previewModal.onOpen(data);
   };
 
-  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
-    event.stopPropagation();
-
-    cart.addItem(data);
-  };
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+      onClick={onPreview}
+    >
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <div className="aspect-square rounded-xl bg-gray-100 relative mb-4">
           <Gallery id={data.sku} urls={data.images} />
-        </div>
-        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
-          <div className="flex gap-x-6 justify-center">
-            <IconButton
-              onClick={onPreview}
-              icon={<Expand size={20} className="text-gray-600" />}
-            />
-            <IconButton
-              onClick={onAddToCart}
-              icon={<ShoppingCart size={20} className="text-gray-600" />}
-            />
-          </div>
         </div>
       </div>
       {/* Name */}
@@ -50,7 +36,7 @@ const ProductItemCard: React.FC<ProductItemCardProps> = ({ data }) => {
         <h2 className="text-lg text-stone-800">{data.itemName}</h2>
       </div>
       {/* Price */}
-        <Currency value={data?.price} discountPrice={data.discountPrice} />
+      <Currency value={data?.price} discountPrice={data.discountPrice} />
       {/* Description */}
       <div>
         <p className="text-sm text-gray-500">stock: {data.qtyInStock}</p>
