400ab194332d65aefcb06dff86360758478aec44
shion0625 <xkaito0912@gmail.com>
2023-11-28 13:23:08 +0800

    cartページにvariationの表示

--- frontend/app/(routes)/cart/components/cart-item.tsx
+++ frontend/app/(routes)/cart/components/cart-item.tsx
@@ -5,10 +5,11 @@ import { X } from "lucide-react";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
-import { Product } from "@/types";
+import { ProductItem } from "@/types";
+import Gallery from "@/components/gallery";
 
 interface CartItemProps {
-  data: Product;
+  data: ProductItem;
 }
 
 const CartItem: React.FC<CartItemProps> = ({ data }) => {
@@ -21,13 +22,7 @@ const CartItem: React.FC<CartItemProps> = ({ data }) => {
   return (
     <li className="flex py-6 border-b">
       <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
-        <NextImage
-          fill
-          sizes="(max-width: 600px) 100vw, 600px"
-          src={data.image}
-          alt=""
-          className="object-cover object-center"
-        />
+        <Gallery id={data.sku} urls={data.images} />
       </div>
       <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6">
         <div className="absolute z-10 right-0 top-0">
@@ -39,8 +34,14 @@ const CartItem: React.FC<CartItemProps> = ({ data }) => {
           </div>
 
           <div className="mt-1 flex text-sm">
-            {/* <p className="text-gray-500">{data.color.name}</p>
-            <p className="ml-4 border-l border-gray-200 pl-4 text-gray-500">{data.size.name}</p> */}
+            {data.variationValues.map((variation) => (
+              <p
+                key={data.name + variation.variationId}
+                className="text-gray-500"
+              >
+                {variation.name}: {variation.value}
+              </p>
+            ))}
           </div>
           <Currency value={data.price} />
         </div>
