59c3e7216e886d1ac170301a5ff3c8ccca2ccc8e
shion0625 <xkaito0912@gmail.com>
2024-01-02 11:45:56 +0800

    一部価格の表示を削除

--- frontend/components/product/product-card.tsx
+++ frontend/components/product/product-card.tsx
@@ -2,7 +2,6 @@
 
 import NextImage from 'next/image';
 import { useRouter } from 'next/navigation';
-import Currency from '@/components/ui/currency';
 import { Product } from '@/types';
 
 interface ProductCard {
